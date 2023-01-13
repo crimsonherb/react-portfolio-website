321a8f8fb30f1e32bf77f190d7d8598292685760
Joma Villanueva <villanueva.joma@gmail.com>
2023-01-13 17:33:55 +0800

    update to portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import IMG1 from '../../assets/Recipe_webapp.jpg'
 import IMG2 from '../../assets/Todolist.jpg'
-import IMG3 from '../../assets/Portfolio3.jpg'
+import IMG3 from '../../assets/PortfolioPage.jpg'
 import IMG4 from '../../assets/DonationPage.jpg'
 import './portfolio.css'
 
